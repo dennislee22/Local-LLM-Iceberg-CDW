e8b823cc9c3913ea2da98fdf52d3a82f15061724
dennislee22 <35444414+dennislee22@users.noreply.github.com>
2025-08-18 19:55:17 +0800

    Update run-fastapi.py

--- run-fastapi.py
+++ run-fastapi.py
@@ -1,3 +1,3 @@
 import os
 CDSW_APP_PORT=os.environ['CDSW_APP_PORT'] 
-os.system("python fastapi-llm.py --server-name=127.0.0.1 --checkpoint-path=Qwen2.5-7B-Instruct --server-port=$CDSW_APP_PORT > fastapi.log 2>&1")
+os.system("python fastapi-llm.py --server-name=127.0.0.1 --checkpoint-path=Qwen2.5-14B-Instruct-1M --server-port=$CDSW_APP_PORT > fastapi.log 2>&1")
