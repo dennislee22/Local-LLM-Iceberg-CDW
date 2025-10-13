5654fde65a2bdacea2f30f1a25f577b18d73c5e9
dennislee22 <35444414+dennislee22@users.noreply.github.com>
2025-10-13 14:22:47 +0800

    Update create_iceberg.py

--- create_iceberg.py
+++ create_iceberg.py
@@ -14,7 +14,7 @@ IMPALA_HOST = 'coordinator-ares-impala-vw.apps.cdppvc.ares.olympus.cloudera.com'
 IMPALA_PORT = 443
 USERNAME = 'dennislee'
 # IMPORTANT: Use an environment variable for the password for better security.
-PASSWORD = os.environ.get('IMPALA_PASSWORD', 'YEEI0oy8BFnSRpwm') # Fallback
+PASSWORD = os.environ.get('IMPALA_PASSWORD', 'blah')
 HTTP_PATH = '/cliservice'
 ICEBERG_DATABASE = "dlee_telco"
 
